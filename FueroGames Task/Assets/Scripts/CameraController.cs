7bda8ea57221c71bcbe7b78398b054a44ae56fc9
Alex <alexander.wozniak1@gmail.com>
2019-06-06 10:59:44 +0200

    camera controller

---
+++ FueroGames Task/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public float cameraFollowSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = playerTransform.position;
+        target.z = -10;
+
+        transform.position = Vector3.Lerp(transform.position, target, cameraFollowSpeed * Time.deltaTime);
+    }
+}
